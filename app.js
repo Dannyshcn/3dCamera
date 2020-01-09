77439cea70a9617523bc31b5ab6ea724e6917364
lip-realmax <56542376+lip-realmax@users.noreply.github.com>
2020-01-09 18:07:12 +0800

    Update pi cam images

--- app.js
+++ app.js
@@ -337,13 +337,14 @@ function execute( cmd, callback ) {
 
 function takeImage() {
     var args = [
-        //'-w', 2592,   // width
-        //'-h', 1944,  // height
+        '-w', 2460,   // width
+        '-h', 1848,  // height
         //'-t', 100,  // how long should taking the picture take?
         '-q', 100,     // quality
 	'-fli', 'auto',	//Anti flickering
 	'-gps',
 	//'-vf',		//Vertial flip
+	'-roi', '0.5,0.5,0.75,0.75',
 	'-r',		//Raw layer
         '-awb', 'incandescent', //'fluorescent', 
         '-o', getAbsoluteImagePath()   // path + name
