ada7d25531a9f73b405b5201f11f4773d454b72b
lip-realmax <56542376+lip-realmax@users.noreply.github.com>
2020-02-06 16:14:11 +0800

    Update pi_capture.py

--- pi_capture.py
+++ pi_capture.py
@@ -11,8 +11,8 @@ waitTime = float( sys.argv[2] ) * 0.001
 folder = sys.argv[3];
 with picamera.PiCamera(resolution=(3280, 2464), framerate=15) as camera:
   camera.shutter_speed = 20000
-  camera.iso = 80
-  camera.awb_mode = 'incandescent'
+  camera.iso = 60
+  camera.awb_mode = 'flash'
   camera.exposure_mode = 'sports'
   #camera.awb_gains = [Fraction(173,128), Fraction(591, 256)]
   camera.start_preview()
